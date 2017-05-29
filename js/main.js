72da9e53a581992951b201427cd3fffae7cfecff
ferlargut <ferlargut@gmail.com>
2017-05-29 16:32:49 -0500

    Entendiendo

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready()
+
+
+$('.contenedor div:gt(0)').hide(); 
+$("#img1").click(function () {
+    $("#imagen1").fadeIn("slow"); 
+    $(".carrusel").not("#imagen1").fadeOut("3000");
+})
+$("#img2").click(function () {
+    $("#imagen2").fadeIn("slow");
+    $(".carrusel").not("#imagen2").fadeOut("3000");      
+})
+$("#img3").click(function () {
+    $("#imagen3").fadeIn("slow");
+    $(".carrusel").not("#imagen3").fadeOut("3000");       
+})
+$("#img4").click(function () {
+    $("#imagen4").fadeIn("slow");
+    $(".carrusel").not("#imagen4").fadeOut("3000");       
+})
